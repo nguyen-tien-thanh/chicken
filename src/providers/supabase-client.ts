108602275ba4e49bcba562fa42d93a6596a189dd
Nguyen Tien Thanh <thanhnt2@vietmap.vn>
2026-05-12 15:21:53 +0700

    fix: update environment variable access for Supabase client

--- src/providers/supabase-client.ts
+++ src/providers/supabase-client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@refinedev/supabase";
 import type { SupabaseClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.VITE_API_URL!;
-const SUPABASE_KEY = process.env.VITE_SUPABASE_API_KEY!;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL!;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY!;
 
 export const supabaseClient: SupabaseClient = createClient(
   SUPABASE_URL,
