5243f39dfbfd997fbca4089529ed4bc05a9ffad0
Nguyen Tien Thanh <thanhnt2@vietmap.vn>
2026-05-12 15:19:06 +0700

    refactor: use env

--- src/providers/supabase-client.ts
+++ src/providers/supabase-client.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@refinedev/supabase";
 import type { SupabaseClient } from "@supabase/supabase-js";
-import { SUPABASE_KEY, SUPABASE_URL } from "./constants";
+
+const SUPABASE_URL = process.env.VITE_API_URL!;
+const SUPABASE_KEY = process.env.VITE_SUPABASE_API_KEY!;
 
 export const supabaseClient: SupabaseClient = createClient(
   SUPABASE_URL,
